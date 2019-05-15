4f17145e63b64431a2f49a5554fc362163b9c280
VuNguyen0711 <nguyentrananhvu88@gmail.com>
2019-05-15 14:33:44 +0700

    add more repo

--- TeduShop.Data/Repositories/ProductRepository.cs
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -5,12 +5,14 @@ namespace TeduShop.Data.Repositories
 {
     public interface IProductRepository
     {
+
     }
 
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+
         }
     }
 }
