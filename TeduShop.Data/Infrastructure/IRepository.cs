e1363f4ed6770ece937e4924747b9a369fcc06a4
VuNguyen0711 <nguyentrananhvu88@gmail.com>
2019-05-15 16:21:13 +0700

    commit bai 8

--- TeduShop.Data/Infrastructure/IRepository.cs
+++ TeduShop.Data/Infrastructure/IRepository.cs
@@ -15,7 +15,7 @@ namespace TeduShop.Data.Infrastructure
         // Marks an entity to be removed
         void Delete(T entity);
 
-        //T Delete(int id);
+        void Delete(int id);
 
         //Delete multi records
         void DeleteMulti(Expression<Func<T, bool>> where);
