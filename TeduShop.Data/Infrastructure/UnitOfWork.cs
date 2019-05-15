eb37ab238ef1af274ee66b26a44a47dae039b14e
VuNguyen0711 <nguyentrananhvu88@gmail.com>
2019-05-15 13:33:47 +0700

    commit bai 6

--- TeduShop.Data/Infrastructure/UnitOfWork.cs
+++ TeduShop.Data/Infrastructure/UnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TeduShop.Data.Infrastructure
+﻿namespace TeduShop.Data.Infrastructure
 {
     public class UnitOfWork : IUnitOfWork
     {
@@ -20,6 +14,7 @@ namespace TeduShop.Data.Infrastructure
         {
             get { return dbContext ?? (dbContext = dbFactory.Init()); }
         }
+
         public void Commit()
         {
             DbContext.SaveChanges();
